c630cb9f1e00b6223578cac07c8e5b4712c0cb51
EricPuig10 <puigvendrelloetona@gmail.com>
2022-07-20 19:27:33 +0200

    15 productos añadidos a base de datos

--- fakemarketplace/src/main/resources/data.sql
+++ fakemarketplace/src/main/resources/data.sql
@@ -3,14 +3,41 @@ INTO
   users
   (name, img)
 VALUES
-  ('JohnDoe', 'https://cdn-icons-png.flaticon.com/512/21/21104.png');
+  ('Chigre', 'https://cdn-icons-png.flaticon.com/512/21/21104.png');
+
+  INSERT
+  INTO
+    users
+    (name, img)
+  VALUES
+    ('Pancho Porto', 'https://cdn-icons-png.flaticon.com/512/21/21104.png');
+  INSERT
+  INTO
+    users
+    (name, img)
+  VALUES
+    ('Flod', 'https://cdn-icons-png.flaticon.com/512/21/21104.png');
+
+  INSERT
+  INTO
+    users
+    (name, img)
+  VALUES
+    ('Benito Leal Gallardo', 'https://cdn-icons-png.flaticon.com/512/21/21104.png');
 
 INSERT
-INTO
-  users
-  (name, img)
-VALUES
-  ('JahnDoa', 'https://cdn-icons-png.flaticon.com/512/21/21104.png');
+  INTO
+    users
+    (name, img)
+  VALUES
+    ('Marisol Evora', 'https://cdn-icons-png.flaticon.com/512/21/21104.png');
+
+INSERT
+  INTO
+    users
+    (name, img)
+  VALUES
+    ('Alvaro Garcia Lema', 'https://cdn-icons-png.flaticon.com/512/21/21104.png');
 
 
 
@@ -19,102 +46,83 @@ INTO
   products
   (name, category, price, img, description, seller_id)
 VALUES
-  ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cloudfront-us-east-1.images.arcpublishing.com/culturacolectiva/KUFCDXCONRDCHFF5Z4XVP6DZKM.jpg', 'I m a little description', 1);
+  ('Vaqueros de la serie Alma Cantaro', 'Escultura', 1637, 'https://www.artmajeur.com/medias/mini/c/h/chigre/artwork/15622252_baqueros-1.jpg', 'Obra realizada en arcilla cocida y esmaltada', 1);
 
   INSERT
   INTO
     products
     (name, category, price, img, description, seller_id)
   VALUES
-    ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cloudfront-us-east-1.images.arcpublishing.com/culturacolectiva/KUFCDXCONRDCHFF5Z4XVP6DZKM.jpg', 'I m a little description', 1);
+    ('Totos', 'Escultura', 1334, 'https://www.artmajeur.com/medias/mini/c/h/chigre/artwork/15622525_totos-2.jpg', 'Obra realizada con ferralla de torno conformada y pintada con pintura al polvo', 1);
     INSERT
     INTO
       products
       (name, category, price, img, description, seller_id)
     VALUES
-      ('Eric Cuadro', 'Cuadros', 2, 'https://cloudfront-us-east-1.images.arcpublishing.com/culturacolectiva/KUFCDXCONRDCHFF5Z4XVP6DZKM.jpg', 'I m a little description', 1);
+      ('Dias de lluvia', 'Escultura', 171, 'https://www.artmajeur.com/medias/mini/f/r/francisco-porto-paredes/artwork/9972841_20170109-21-e.jpg', 'Escultura en bronce y acero', 2);
       INSERT
       INTO
         products
         (name, category, price, img, description, seller_id)
       VALUES
-        ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cloudfront-us-east-1.images.arcpublishing.com/culturacolectiva/KUFCDXCONRDCHFF5Z4XVP6DZKM.jpg', 'I m a little description', 1);
+        ('El vengador de afrentas', 'Escultura', 138, 'https://www.artmajeur.com/medias/mini/f/r/francisco-porto-paredes/artwork/9972784_20170109-29.jpg', 'Escultura en bronce y acero', 2);
         INSERT
         INTO
           products
           (name, category, price, img, description, seller_id)
         VALUES
-          ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cloudfront-us-east-1.images.arcpublishing.com/culturacolectiva/KUFCDXCONRDCHFF5Z4XVP6DZKM.jpg', 'I m a little description', 1);
+          ('Gaia dans une foret de kelp', 'Cuadros', 3594, 'https://www.artmajeur.com/medias/mini/f/l/flodouce/artwork/15210592_gaia-9.jpg?v=1640456702', 'Pintura con pigmentos tradicionales mezclados con resinas', 3);
           INSERT
           INTO
             products
             (name, category, price, img, description, seller_id)
           VALUES
-            ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cdn-icons-png.flaticon.com/512/21/21104.png', 'I m a little description', 1);
+            ('Magic land', 'Cuadros', 546, 'https://www.artmajeur.com/medias/mini/f/l/flodouce/artwork/15647398_magic-land-3-b.jpg?v=1649958519', 'Pintura, acrílico en lienzo de lino', 3);
             INSERT
             INTO
               products
               (name, category, price, img, description, seller_id)
             VALUES
-              ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cdn-icons-png.flaticon.com/512/21/21104.png', 'I m a little description', 1);
+              ('Migrantes', 'Cuadros', 3503, 'https://www.artmajeur.com/medias/mini/a/n/ancalatita/artwork/14484845_migrantes.jpg', 'Pintura,acrílico en lienzo', 4);
               INSERT
               INTO
                 products
                 (name, category, price, img, description, seller_id)
               VALUES
-                ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cdn-icons-png.flaticon.com/512/21/21104.png', 'I m a little description', 1);
+                ('La desforestacion masiva importa un pimiento', 'Cuadros', 1572, 'https://www.artmajeur.com/medias/mini/a/n/ancalatita/artwork/14577305_me-importa-un-pimiento.jpg', 'Pintura, acrílico en madera', 4);
                 INSERT
                 INTO
                   products
                   (name, category, price, img, description, seller_id)
                 VALUES
-                  ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cdn-icons-png.flaticon.com/512/21/21104.png', 'I m a little description', 1);
+                  ('El reflejo de la reflexion de los perros', 'Cuadros', 2.891, 'https://www.artmajeur.com/medias/mini/a/n/ancalatita/artwork/14484686_el-reflejo.jpg?v=1620055943', 'Pintura, oleo en lienzo', 4);
                   INSERT
                   INTO
                     products
                     (name, category, price, img, description, seller_id)
                   VALUES
-                    ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cdn-icons-png.flaticon.com/512/21/21104.png', 'I m a little description', 1);
+                    ('Pretend to be normal', 'Cuadros', 902, 'https://www.artmajeur.com/medias/mini/m/a/marisol-evora/artwork/15838339_pretend-to-be-normal-whole-bild.jpg', 'Pintura,acrilico, rotulador en lienzo', 5);
                     INSERT
                     INTO
                       products
                       (name, category, price, img, description, seller_id)
                     VALUES
-                      ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cdn-icons-png.flaticon.com/512/21/21104.png', 'I m a little description', 1);
+                      ('El alma de la mariposa', 'Cuadros', 2303, 'https://www.artmajeur.com/medias/mini/m/a/marisol-evora/artwork/15837301_la-alma-de-la-mariposa.jpg', 'Pintura, acrilico, rotulador en lienzo', 5);
                       INSERT
                       INTO
                         products
                         (name, category, price, img, description, seller_id)
                       VALUES
-                        ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cdn-icons-png.flaticon.com/512/21/21104.png', 'I m a little description', 1);
+                        ('Happy anywhere', 'Cuadros', 3209, 'https://www.artmajeur.com/medias/mini/m/a/marisol-evora/artwork/15831982_dsc-0217-1.jpg', 'Acrilico en lienzo', 5);
                         INSERT
                         INTO
                           products
                           (name, category, price, img, description, seller_id)
                         VALUES
-                          ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cdn-icons-png.flaticon.com/512/21/21104.png', 'I m a little description', 1);
+                          ('En red', 'Grabados', 535, 'https://www.artmajeur.com/medias/mini/a/l/alvaro-garcia-lema/artwork/15909763_en-red.jpg?v=1658085838', 'Xilografia en papel', 6);
                           INSERT
                           INTO
                             products
                             (name, category, price, img, description, seller_id)
                           VALUES
-                            ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cdn-icons-png.flaticon.com/512/21/21104.png', 'I m a little description', 1);
-                            INSERT
-                            INTO
-                              products
-                              (name, category, price, img, description, seller_id)
-                            VALUES
-                              ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cdn-icons-png.flaticon.com/512/21/21104.png', 'I m a little description', 1);
-                              INSERT
-                              INTO
-                                products
-                                (name, category, price, img, description, seller_id)
-                              VALUES
-                                ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cdn-icons-png.flaticon.com/512/21/21104.png', 'I m a little description', 1);
-                                INSERT
-                                INTO
-                                  products
-                                  (name, category, price, img, description, seller_id)
-                                VALUES
-                                  ('Cuadro Van Gogh', 'Cuadros', 2, 'https://cdn-icons-png.flaticon.com/512/21/21104.png', 'I m a little description', 1);
-
+                            ('Lagarto, lagarto', 'Grabados', 235, 'https://www.artmajeur.com/medias/mini/a/l/alvaro-garcia-lema/artwork/15909760_lagarto-lagarto.jpg?v=1658078633', 'Xilografia en papel', 6);
